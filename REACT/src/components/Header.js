e2ab761d8be448b052723238016542a719dbc644
Gilang pRasetya <gilangprasetya9@gmail.com>
2023-07-27 09:16:36 +0700

    crud done

--- REACT/src/components/Header.js
+++ REACT/src/components/Header.js
@@ -1,20 +1,75 @@
-export default function Header() {
-    return(
-    <div className="top-bar">
-        <div className="item">
-            <button className="filter">
-                <i className="fa-solid fa-arrow-up-z-a"></i>
-            </button>
+import { useState } from "react";
+
+export default function Header({ handleAddContact }) {
+    const [showPopup, setShowPopup] = useState(false);
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleAddButtonClick = () => {
+        setShowPopup(true);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setShowPopup(false);
+        handleAddContact(name, phone);
+        setName("");
+        setPhone("");
+        window.location.reload();
+    };
+
+    const handleCancelButtonClick = () => {
+        setShowPopup(false);
+        setName("");
+        setPhone("");
+    };
+
+    return (
+        <div className="top-bar">
+            <div className="item">
+                <button className="filter">
+                    <i className="fa-solid fa-arrow-up-z-a"></i>
+                </button>
+            </div>
+            <div className="item search-form">
+                <i className="fas fa-solid fa-magnifying-glass"></i>
+                <input type="text" id="search" className="search" />
+            </div>
+            <div className="item">
+                <button
+                    className="add"
+                    onClick={handleAddButtonClick}
+                    onMouseOver={(e) => (e.target.style.cursor = 'pointer')}
+                    onMouseOut={(e) => (e.target.style.cursor = 'auto')}>
+                    <i className="fa-solid fa-user-plus"></i>
+                </button>
+            </div>
+
+            {/* Popup menu */}
+            {showPopup && (
+                <div className="popup">
+                    <form onSubmit={handleFormSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Phone"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                        <div className="button-add">
+                            <button type="submit">Save</button>
+                            <button type="button" onClick={handleCancelButtonClick}>
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            )}
         </div>
-        <div className="item search-form">
-            <i className="fas fa-solid fa-magnifying-glass"></i>
-            <input type="text" id="search" className="search" />
-        </div>
-        <div className="item">
-            <button className="add">
-                <i className="fa-solid fa-user-plus"></i>
-            </button>
-        </div>
-    </div>
-    )
+    );
 }
--- REACT/src/components/Phonebooks.js
+++ REACT/src/components/Phonebooks.js
@@ -15,10 +15,31 @@ export default function Phonebooks() {
         });
     }, []);
 
+    const handleAddContact = async (name, phone) => {
+        try {
+            // Send a POST request to the server to create the new contact
+            const response = await axios.post('http://localhost:3001/api/phonebooks', {
+                name: name,
+                phone: phone
+            });
+
+            // Get the newly created contact data from the server response
+            const newContact = response.data;
+
+            // Update the data state with the new contact
+            setData((prevData) => [...prevData, newContact]);
+        } catch (error) {
+            console.error("Error creating contact:", error);
+        }
+    };
+
     return (
         <div className="container">
             <header>
-                <Header />
+                <header>
+                    {/* Pass the handleAddContact function as a prop */}
+                    <Header handleAddContact={handleAddContact} />
+                </header>
             </header>
             <main className="mt-3">
                 <ul>
